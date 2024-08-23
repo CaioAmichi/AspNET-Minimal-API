9ff7f9945dc169393a796555fc414f2be91b2ca3
seupu <seupu@DESKTOP-JCV8NTT>
2024-08-23 16:06:54 -0300

    Add project files.

---
+++ RangoAgil.API/Entitites/Ingredient.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RangoAgil.API.Entitites;
+
+public class Ingredient
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public required string Name { get; set; }
+    public Collection<Meal> Meals { get; set; } = [];
+
+    public Ingredient()
+    {
+            
+    }
+
+    [SetsRequiredMembers]
+    public Ingredient(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
+
---
+++ RangoAgil.API/Entitites/Meal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RangoAgil.API.Entitites;
+
+public class Meal
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public required string Name { get; set; }
+    public Collection<Ingredient> Ingredients { get; set; } = [];
+    public Meal()
+    {
+
+    }
+
+    [SetsRequiredMembers]
+    public Meal(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
+
